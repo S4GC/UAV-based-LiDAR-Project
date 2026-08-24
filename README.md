9cdd92d55a7945f415f5f91a9ec031ee9c3b5824
Santiago <santy@new-horizons>
2026-08-24 16:51:22 -0500

    Reorganization

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # UAV-based-LiDAR-Project
-UAV based LiDAR Project
+
+An integrated embedded hardware and simulation pipeline designed for UAV LiDAR payload operations. This repository contains Arduino drivers for low-level sensor reading, ESP-based firmware for real-time web server data streaming, and a Python visualizer for modeling 2-DOF gimbal scanning trajectory patterns.
--- Visualization/GimballAnimation.py
+++ Visualization/GimballAnimation.py
@@ -2,18 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Parámetros
-R = 1.0
-n = 10   # resolución angular -> total ~ n^2 puntos
-frames = n * n
 
 # -------------------------------
-# 1. Esféricas estándar (arriba->abajo, izq->der)
+# Parameters
 # -------------------------------
-theta = np.linspace(-np.pi/2, np.pi/2, n)      # polar
-phi = np.linspace(0, np.pi, n)          # azimutal 0-180°
-psi = np.linspace(0, 2*np.pi, 2*n)  # azimutal 0-360°
-gamma = np.linspace(0, 0.5*np.pi, n//2)  # giro sobre el eje
+
+R = 1.0
+n = 10   # Angular resolution -> Total n^2 points
+frames = n * n
+
+
+# -------------------------------
+# Variables
+# -------------------------------
+
+theta = np.linspace(-np.pi/2, np.pi/2, n) # Polar
+phi = np.linspace(0, np.pi, n) # Azimutal 0-180°
+psi = np.linspace(0, 2*np.pi, 2*n)  # Azimutal 0-360°
+gamma = np.linspace(0, 0.5*np.pi, n//2)  # Gyro around axis
 
 theta, phi = np.meshgrid(theta, phi)
 psi, gamma = np.meshgrid(psi, gamma)
@@ -33,9 +39,11 @@ y3 = R * np.cos(gamma) * np.sin(psi)
 z3 = R * np.sin(gamma)
 points3 = np.c_[x3.ravel(), y3.ravel(), z3.ravel()]
 
+
 # -------------------------------
-# Configuración de gráficas
+# Plot settings
 # -------------------------------
+
 fig = plt.figure(figsize=(12, 4))
 axes = [fig.add_subplot(131, projection="3d"),
         fig.add_subplot(132, projection="3d"),
@@ -58,6 +66,7 @@ for ax, title in zip(axes, titles):
 # -------------------------------
 # Función de animación
 # -------------------------------
+
 def update(frame):
     for line, pts in zip(scatters, data):
         subset = pts[:frame]
@@ -67,7 +76,11 @@ def update(frame):
 
 anim = FuncAnimation(fig, update, frames=frames, interval=50, blit=False)
 
-# Guardar como GIF
+
+# -------------------------------
+# Save GIF
+# -------------------------------
+
 anim.save("semiesferas_patrones.gif", writer="pillow")
 plt.show()
 
